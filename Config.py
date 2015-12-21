81c53eb83c520f1edad06c1c5c9d52759f5f4c0f
Tomasz Los <Tomasz.Los@smart4aviation.aero>
2015-12-21 11:56:27 +0100

    Added stub for ProjectDAO

--- Config.py
+++ Config.py
@@ -1,6 +1,8 @@
 logs_file_name_prefix = "/home/tomek/tmp/logs_"
 logs_file_append_pid = True
 
+projects_db_path = '/home/tomek/tmp/project_db.json'
+
 # optimize actions to one dic
 
 actions = {
@@ -15,6 +17,38 @@ actions = {
 
 # TODO add autoscan
 projects_paths = {
-    
+    'util-parent': '/home/tomek/workspace/UTILS/util-parent',
+    'nav': '/home/tomek/workspace/CORE/nav',
+    'smart-core': '/home/tomek/workspace/SmartCORE/smart-core',
+    'audit': '/home/tomek/workspace/CORE/audit',
+    'acft': '/home/tomek/workspace/CORE/acft',
+    'flight': '/home/tomek/workspace/CORE/flight',
+    'svcreg': '/home/tomek/workspace/CORE/svcreg',
+
+    'cdm-core': '/home/tomek/workspace/SmartDOC/cdm-core',
+    'cdm-web': '/home/tomek/workspace/SmartDOC/cdm-web',
+    'cdm-iface': '/home/tomek/workspace/SmartDOC/cdm-iface',
+    'smart-doc': '/home/tomek/workspace/SmartDOC/smart-doc',
+    'smart-doc-ear': '/home/tomek/workspace/SmartDOC/smart-doc-ear',
+    'smart-doc-war': '/home/tomek/workspace/SmartDOC/smart-doc-war',
+    'smart-doc-services-war': '/home/tomek/workspace/SmartDOC/smart-doc-war',
+    'smart-doc-settings': '/home/tomek/workspace/SmartDOC/smart-doc-settings',
+    'smart-doc-theme': '/home/tomek/workspace/SmartDOC/smart-doc-theme',
+
+    'airline': '/home/tomek/workspace/CORE/airline',
+
+    'ezy-portal-ear': '/home/tomek/workspace/EZY/ezy-portal-ear',
+    'ezy-doc-ear': '/home/tomek/workspace/EZY/ezy-doc-ear',
+    'ezy-portal-settings': '/home/tomek/workspace/EZY/ezy-portal-settings',
+    'ezy-doc-settings': '/home/tomek/workspace/EZY/ezy-doc-settings',
+    'ezy-core-ear': '/home/tomek/workspace/EZY/ezy-core-ear',
+
+    'pax': '/home/tomek/workspace/CORE/pax',
+    'bootstrap': '/home/tomek/workspace/BOOTSTRAPS/bootstrap',
+
+
+    'smart-um': '/home/tomek/workspace/UM/smart-um',
+
+    'crew': '/home/tomek/workspace/CORE/crew'
 
 }
---
+++ ProjectsDAO.py
@@ -0,0 +1,10 @@
+import json
+import Config
+
+
+def getProjects(db_path=Config.projects_db_path):
+    pass
+
+
+def saveProjects(projects_json, db_path=Config.projects_db_path):
+    pass
